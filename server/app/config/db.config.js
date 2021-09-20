b8e7b7223f3d84371ef2f12a87365cf590602294
deliteser112 <ryndinalex112@gmail.com>
2021-09-20 15:12:40 +0900

    deploy-8

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,21 +1,7 @@
-// module.exports = {
-//   HOST: 'localhost',
-//   USER: 'thimbleuser',
-//   PASSWORD: 'Deliteser112!!@',
-//   DB: 'db_thimble',
-//   dialect: 'mysql',
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
 module.exports = {
   HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: 'root',
+  USER: 'thimbleuser',
+  PASSWORD: 'Deliteser112!!@',
   DB: 'db_thimble',
   dialect: 'mysql',
   pool: {
@@ -25,3 +11,17 @@ module.exports = {
     idle: 10000
   }
 };
+
+// module.exports = {
+//   HOST: 'localhost',
+//   USER: 'root',
+//   PASSWORD: 'root',
+//   DB: 'db_thimble',
+//   dialect: 'mysql',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
