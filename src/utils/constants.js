e46308a6164a53b98c3c356bdf4ad68882b73d4b
deliteser112 <ryndinalex112@gmail.com>
2021-09-24 18:53:26 +0900

    deploy - 12

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 export const CONSTANTS = {
   // serverURL: 'http://54.197.154.252:8081/'
-  serverURL: 'http://localhost:3001'
+  serverURL: 'http://54.197.154.252:3001/'
+  // serverURL: 'http://localhost:3001'
 };
