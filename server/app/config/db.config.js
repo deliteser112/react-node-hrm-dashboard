b6dc0aab7a39263c89566c5eb595397885c22814
deliteser112 <ryndinalex112@gmail.com>
2021-09-26 18:03:07 +0900

    deploy - 14

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -11,17 +11,3 @@ module.exports = {
     idle: 10000
   }
 };
-
-// module.exports = {
-//   HOST: 'localhost',
-//   USER: 'root',
-//   PASSWORD: 'root',
-//   DB: 'db_thimble',
-//   dialect: 'mysql',
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
